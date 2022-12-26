da80adb29a7476e4fba11158b43f778a42fd3218
VipulChauhan89 <90852928+VipulChauhan89@users.noreply.github.com>
2022-12-26 22:47:30 +0530

    Create 0055-Jump_Game.cpp

---
+++ Dynamic Programming/0055-Jump_Game.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) 
+    {
+        int distance=0;
+        for(int i=0;i<nums.size()-1;i++)
+        {
+            distance=max(distance,i+nums[i]);
+            if(distance==i)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
