bb3d3b023f82164dd6ee5747d5de06f303232eee
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2023-02-06 17:30:45 +0530

    Create 1470-Shuffle_the_Array.cpp

--- Array/1470-Shuffle_the_Array.cpp
+++ Array/1470-Shuffle_the_Array.cpp
@@ -2,12 +2,13 @@ class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) 
     {
-        vector<int> v;
-        for(int i=0;i<n;i++)
+        vector<int> ans;
+        int i=0,j=n;
+        while(i<n)
         {
-            v.push_back(nums[i]);
-            v.push_back(nums[n+i]);
+            ans.push_back(nums[i++]);
+            ans.push_back(nums[j++]);
         }
-        return v;
+        return ans;
     }
 };
