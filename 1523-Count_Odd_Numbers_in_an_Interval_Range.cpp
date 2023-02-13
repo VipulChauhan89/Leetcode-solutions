64ccf8cdb1c4c215f49bf75dd2664033f45f26e7
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2023-02-13 21:13:26 +0530

    Create 1523-Count_Odd_Numbers_in_an_Interval_Range.cpp

---
+++ 1523-Count_Odd_Numbers_in_an_Interval_Range.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countOdds(int low, int high) 
+    {
+        int count=0;
+        if(low%2==1 || high%2==1)
+        {
+            count++;
+        }
+        count+=(high-low)/2;
+        return count;
+    }
+};
