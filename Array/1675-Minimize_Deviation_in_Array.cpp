f7d4144a63a194680817959aa949f74149e5056a
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2023-02-24 22:16:36 +0530

    Create 1675-Minimize_Deviation_in_Array.cpp

---
+++ Array/1675-Minimize_Deviation_in_Array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minimumDeviation(vector<int>& nums) 
+    {
+        set<int> st;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]%2!=0)
+            {
+                nums[i]*=2;
+            }
+            st.insert(nums[i]);
+        }
+        int mnDev=*st.rbegin()-*st.begin();
+        while(*st.rbegin()%2==0)
+        {
+            int temp=*st.rbegin();
+            st.erase(temp);
+            temp/=2;
+            st.insert(temp);
+            mnDev=min(mnDev,*st.rbegin()-*st.begin());
+        }   
+        return mnDev;
+    }
+};
