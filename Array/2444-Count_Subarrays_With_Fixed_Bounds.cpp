918be9428df91196cbf2da99b5681f33114dcc66
VipulChauhan89 <vipulchauhan548@gmail.com>
2023-03-04 10:12:21 +0530

    Create 2444-Count_Subarrays_With_Fixed_Bounds.cpp

---
+++ Array/2444-Count_Subarrays_With_Fixed_Bounds.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, int minK, int maxK) 
+    {
+        long long ans=0;
+        int badi=-1,mn=-1,mx=-1,n=nums.size();
+
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]<minK || nums[i]>maxK)
+            {
+                badi=i;
+            }
+            if(nums[i]==minK)
+            {
+                mn=i;
+            }
+            if(nums[i]==maxK)
+            {
+                mx=i;
+            }
+            ans+=max(0,min(mn,mx)-badi);
+        }
+        return ans;
+    }
+};
