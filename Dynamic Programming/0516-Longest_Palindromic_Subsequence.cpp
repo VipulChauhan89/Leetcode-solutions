3a393a7b87200b4ad040d25956c443d33bf83d5a
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2023-04-14 20:38:02 +0530

    Create 0516-Longest_Palindromic_Subsequence.cpp

--- Dynamic Programming/0516-Longest_Palindromic_Subsequence.cpp
+++ Dynamic Programming/0516-Longest_Palindromic_Subsequence.cpp
@@ -4,21 +4,21 @@ public:
     {
 	    int dp[m+1][n+1];
 	    for(int i=0;i<=m;i++)
-        {
+	    {
 		    for(int j=0;j<=n;j++)
-            {
+		    {
 			    if(i==0 || j==0)
-                {
+			    {
 				    dp[i][j]=0;
-                }
+			    }
 			    else if(A[i-1]==B[j-1])
-                {
+			    {
 				    dp[i][j]=1+dp[i-1][j-1]; 
-                }
+			    }
 			    else
-                {
+			    {
 				    dp[i][j]=max(dp[i-1][j],dp[i][j-1]); 
-                }
+			    }
 		    }
 	    }
 	    return dp[m][n];
