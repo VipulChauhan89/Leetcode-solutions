cc436e08d07c0d188239b919b913fc5cce9d047e
VipulChauhan89 <vipulchauhan548@gmail.com>
2023-05-02 13:42:11 +0530

    Create 1822-Sign_of_the_Product_of_an_Array.cpp

---
+++ Math/1822-Sign_of_the_Product_of_an_Array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int arraySign(vector<int> &nums) 
+    {
+        int sign=1;
+        for(auto i:nums)
+        {
+            if(i==0)
+            {
+                return 0;
+            }
+            else if(i<0)
+            {
+                sign*=-1;
+            }
+        }
+        return sign;
+    }
+};
