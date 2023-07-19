e969a387df6ea1d8ff96acf9c455682c137d6005
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2023-07-19 20:23:15 +0530

    Create 2778-Sum_of_Squares_of_Special_Elements.cpp

---
+++ Array/2778-Sum_of_Squares_of_Special_Elements.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int sumOfSquares(vector<int> &nums) 
+    {
+        int sum=0,n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            if(n%(i+1)==0)
+            {
+                sum+=nums[i]*nums[i];
+            }
+        }
+        return sum;
+    }
+};
