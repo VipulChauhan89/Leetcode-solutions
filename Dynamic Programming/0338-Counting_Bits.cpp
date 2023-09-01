a19d235cff921de111c9f93d39dd54a4db971908
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2023-09-01 21:42:20 +0530

    Update 0338-Counting_Bits.cpp

--- Dynamic Programming/0338-Counting_Bits.cpp
+++ Dynamic Programming/0338-Counting_Bits.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
-    vector<int> countBits(int n) 
-    {
-        vector<int> v;
-        for(int i=0;i<=n;i++)
+    vector<int> countBits(int n) {
+        vector<int> ans(n+1,0);
+        for(int i=1;i<=n;i++)
         {
-            int sum=0,num=i;
-            while(num!=0)
-            {
-                sum+=num%2;
-                num/=2;
-            }
-            v.push_back(sum);
+            ans[i]=ans[i>>1]+(i&1);
         }
-        return v;
+        return ans;
     }
 };
