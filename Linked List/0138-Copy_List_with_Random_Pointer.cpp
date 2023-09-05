445a7d60dd57f0db2e0652ffd683194aaceeb713
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2023-09-05 22:42:41 +0530

    Create 0138-Copy_List_with_Random_Pointer.cpp

---
+++ Linked List/0138-Copy_List_with_Random_Pointer.cpp
@@ -0,0 +1,41 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node *copyRandomList(Node *head) 
+    {
+        if(!head)
+        {
+            return nullptr;
+        } 
+        unordered_map<Node*,Node*> m;
+        Node *curr=head;
+        while(curr) 
+        {
+            m[curr]=new Node(curr->val);
+            curr=curr->next;
+        }
+        curr=head;
+        while(curr) 
+        {
+            m[curr]->next=m[curr->next];
+            m[curr]->random=m[curr->random];
+            curr=curr->next;
+        }
+        return m[head];
+    }
+};
