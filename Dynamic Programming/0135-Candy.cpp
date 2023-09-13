3b6bd55a8cbcca03b2ecf7967859f6489bac00f3
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2023-09-13 22:27:49 +0530

    Create 0135-Candy.cpp

---
+++ Dynamic Programming/0135-Candy.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+  int candy(vector<int> &ratings) 
+  {
+    int n=ratings.size();
+    vector<int> candies(n,1);
+    for(int i=1;i<n;i++) 
+    {
+      if(ratings[i]>ratings[i-1] && candies[i]<=candies[i-1]) 
+      {
+        candies[i]=candies[i - 1] + 1;
+      }
+    }
+    for(int i=n-2;i>=0;i--) 
+    {
+      if(ratings[i]>ratings[i+1] && candies[i]<=candies[i+1]) 
+      {
+        candies[i]=candies[i+1]+1;
+      }
+    }
+    int total=0;
+    for(int i=0;i<n;i++) 
+    {
+      total+=candies[i];
+    }
+    return total;
+  }
+};
