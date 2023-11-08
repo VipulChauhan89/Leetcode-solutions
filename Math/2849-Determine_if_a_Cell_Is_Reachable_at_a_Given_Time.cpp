197d7af393111a3dc1427398f407d4190b0cb654
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2023-11-08 15:02:06 +0530

    Create 2849-Determine_if_a_Cell_Is_Reachable_at_a_Given_Time.cpp

---
+++ Math/2849-Determine_if_a_Cell_Is_Reachable_at_a_Given_Time.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool isReachableAtTime(int sx,int sy,int fx,int fy,int t) 
+    {
+        int minStep=max(abs(sx-fx),abs(sy-fy));
+        return (minStep==0)?t!=1:minStep<=t;
+    }
+};
