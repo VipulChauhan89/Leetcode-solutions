1e29a5edc8e5a94fbdbbe0d1e5f6abf2812111f7
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2023-12-14 18:14:11 +0530

    Create 2482-Difference_Between_Ones_and_Zeros_in_Row_and_Column.cpp

---
+++ Array/2482-Difference_Between_Ones_and_Zeros_in_Row_and_Column.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> onesMinusZeros(vector<vector<int>> &grid) 
+    {
+        int m=grid.size(),n=grid[0].size();
+        vector<int> rowOnes(m,0),colOnes(n,0);
+        for(int i=0;i<m;i++) 
+        {
+            for(int j=0;j<n;j++) 
+            {
+                rowOnes[i]+=grid[i][j];
+                colOnes[j]+=grid[i][j];
+            }
+        }
+        for(int i=0;i<m;i++) 
+        {
+            for(int j=0;j<n;j++) 
+            {
+                grid[i][j]=2*(rowOnes[i]+colOnes[j])-m-n;
+            }
+        }
+        return grid;
+    }
+};
