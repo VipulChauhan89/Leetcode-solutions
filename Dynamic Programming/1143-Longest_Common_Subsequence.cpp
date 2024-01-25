3ac639718950e478a5ac729fc86f3137dea7be71
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2024-01-25 20:05:18 +0530

    Update 1143-Longest_Common_Subsequence.cpp

--- Dynamic Programming/1143-Longest_Common_Subsequence.cpp
+++ Dynamic Programming/1143-Longest_Common_Subsequence.cpp
@@ -1,27 +1,16 @@
 class Solution {
 public:
-    int longestCommonSubsequence(string text1, string text2) 
+    int longestCommonSubsequence(string text1,string text2) 
     {
-        int n=text1.length(),m=text2.length();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-        for(int i=0;i<=n;i++)
+        int t1Len=text1.length(),t2Len=text2.length();
+        vector<vector<int>> dp(t1Len+1,vector<int>(t2Len+1,0));
+        for(int i=1;i<=t1Len;i++)
         {
-            for(int j=0;j<=m;j++)
+            for(int j=1;j<=t2Len;j++)
             {
-                if(i==0||j==0)
-                {
-                    dp[i][j]=0;
-                }
-                else if(text1[i-1]==text2[j-1])
-                {
-                    dp[i][j]=1+dp[i-1][j-1];
-                }
-                else
-                {
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-                }
+                dp[i][j]=((text1[i-1]==text2[j-1])?dp[i-1][j-1]+1:max(dp[i-1][j],dp[i][j-1]));
             }
         }
-        return dp[n][m];
+        return dp[t1Len][t2Len];
     }
 };
