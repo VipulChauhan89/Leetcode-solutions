bc418636c77a8a6b7bfbe29fe8e436da24a2b396
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2024-02-21 10:56:28 +0530

    Create 0201-Bitwise_AND_of_Numbers_Range.cpp

---
+++ Bit Manipulation/0201-Bitwise_AND_of_Numbers_Range.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int rangeBitwiseAnd(int left,int right) 
+    {
+        while(left<right)
+        {
+            right&=right-1;
+        }    
+        return right;
+    }
+};
