2211922159301264b8a8943079eff53e45df17c1
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2024-03-02 13:39:10 +0530

    Update 0977-Squares_of_a_Sorted_Array.cpp

--- Array/0977-Squares_of_a_Sorted_Array.cpp
+++ Array/0977-Squares_of_a_Sorted_Array.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
-    static int comp(int &a,int &b)
+    vector<int> sortedSquares(vector<int> &nums) 
     {
-        return a*a<b*b;
-    }
-    vector<int> sortedSquares(vector<int>& nums) 
-    {
-        sort(nums.begin(),nums.end(),comp);
         for(int i=0;i<nums.size();i++)
         {
             nums[i]=nums[i]*nums[i];
         }
+        sort(nums.begin(),nums.end());
         return nums;
     }
 };
