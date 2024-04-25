eb03a0f3dc412e77b41491dc1f8cef7cffde31a0
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2024-04-25 16:42:20 +0530

    Create 2370-Longest_Ideal_Subsequence.cpp

---
+++ Dynamic Programming/2370-Longest_Ideal_Subsequence.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int longestIdealString(string s,int k) 
+    {
+        vector<int> dp(27,0);
+        int n=s.length();
+        for(int i=n-1;i>=0;i--) 
+        {
+            char cc=s[i];
+            int idx=cc-'a',maxi=-__INT_MAX__;
+            int left=max((idx-k),0),right=min((idx+k),26);
+            for(int j=left;j<=right;j++) 
+            {
+                maxi=max(maxi, dp[j]);
+            }
+            dp[idx]=maxi+1;
+        }
+        int max=-__INT_MAX__;
+        for(int i=0;i<27;i++) 
+        {
+            if(dp[i]>max)
+            {
+                max=dp[i];
+            }
+        }
+        return max;
+    }
+};
