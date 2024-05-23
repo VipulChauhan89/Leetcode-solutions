b0138139556a9a51a8f0322625c43a9cc399df44
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2024-05-23 14:06:29 +0530

    Create 2597-The_Number_of_Beautiful_Subsets.cpp

---
+++ Array/2597-The_Number_of_Beautiful_Subsets.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    char freq[1001]={0};
+    int cnt=0,n;
+    void backtrack(int idx,vector<int> &nums,int k) 
+    {
+        if(idx==n) 
+        {
+            cnt++;
+            return;
+        }
+        backtrack(idx+1,nums,k);
+        int x=nums[idx];
+        if((x-k<0 || freq[x-k]==0) && (x+k>1000 || freq[x+k]==0)) 
+        {
+            freq[x]++;
+            backtrack(idx+1,nums,k);
+            freq[x]--;
+        }
+    }
+    int beautifulSubsets(vector<int> &nums,int k) 
+    {
+        n=nums.size();
+        sort(nums.begin(), nums.end());
+        backtrack(0, nums, k);
+        return cnt-1;
+    }
+};
