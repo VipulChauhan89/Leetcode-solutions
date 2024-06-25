1e55d28668d5e9789d19c4abfe43f3019f600663
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2024-06-25 18:32:24 +0530

    Create 1038-Binary_Search_Tree_to_Greater_Sum_Tree.cpp

---
+++ Binary Search Tree/1038-Binary_Search_Tree_to_Greater_Sum_Tree.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void dfs(TreeNode *root,int &sum)
+    {
+        if(root!=NULL)
+        {
+            dfs(root->right,sum);
+            sum+=root->val;
+            root->val=sum;
+            dfs(root->left,sum);
+        }
+    }
+    TreeNode *bstToGst(TreeNode *root) 
+    {
+        int sum=0;
+        dfs(root,sum);
+        return root;
+    }
+};
