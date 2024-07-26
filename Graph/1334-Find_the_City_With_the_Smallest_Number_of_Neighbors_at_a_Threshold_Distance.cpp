ec192979a49b913bc89ae47cf6c1019bb59dd6e0
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2024-07-26 19:36:53 +0530

    Create 1334-Find_the_City_With_the_Smallest_Number_of_Neighbors_at_a_Threshold_Distance.cpp

---
+++ Graph/1334-Find_the_City_With_the_Smallest_Number_of_Neighbors_at_a_Threshold_Distance.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        vector<vector<int>> distance(n,vector<int>(n,1000000000));
+        int ans=-1,mini=INT_MAX;
+        for(int i=0;i<n;i++) 
+        {
+            distance[i][i]=0;
+        }
+        for(auto &i:edges) 
+        {
+            distance[i[0]][i[1]]=i[2];
+            distance[i[1]][i[0]]=i[2];
+        }
+        for(int k=0;k<n;k++) 
+        {
+            for(int i=0;i<n;i++) 
+            {
+                for(int j=0;j<n;j++) 
+                {
+                    distance[i][j]=min(distance[i][j],distance[i][k]+distance[k][j]);
+                }
+            }
+        }
+        for(int i=0;i<n;i++) 
+        {
+            int count=0;
+            for(int j=0;j<n;j++) 
+            {
+                if(i!=j && distance[i][j]<=distanceThreshold) 
+                {
+                    count++;
+                }
+            }
+            if(count<=mini) 
+            {
+                mini=count;
+                ans=i;
+            }
+        }
+        return ans;
+    }
+};
