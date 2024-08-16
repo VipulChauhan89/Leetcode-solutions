1b3e599cbfb329f3594265b18cc4171d0581ba35
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2024-08-16 20:16:08 +0530

    Create 0624-Maximum_Distance_in_Arrays.cpp

---
+++ Array/0624-Maximum_Distance_in_Arrays.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxDistance(vector<vector<int>>& arrays) {
+        int smallest=arrays[0][0],biggest=arrays[0].back(),max_distance=0;
+        for(int i=1;i<arrays.size();i++) 
+        {
+            max_distance=max(max_distance,abs(arrays[i].back()-smallest));
+            max_distance=max(max_distance,abs(biggest-arrays[i][0]));
+            smallest=min(smallest,arrays[i][0]);
+            biggest=max(biggest,arrays[i].back());
+        }
+        return max_distance;
+    }
+};
