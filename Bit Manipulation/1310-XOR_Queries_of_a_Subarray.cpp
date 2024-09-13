f159caa6e615a48584a9025c679a56720d3d8825
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2024-09-13 23:28:59 +0530

    Create 1310-XOR_Queries_of_a_Subarray.cpp

---
+++ Bit Manipulation/1310-XOR_Queries_of_a_Subarray.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> xorQueries(vector<int> &arr,vector<vector<int>> &queries) 
+    {
+        int n=arr.size();
+        vector<int> pre(n);
+        pre[0]=arr[0];
+        for(int i=1;i<n;i++) 
+        {
+            pre[i]=pre[i-1]^arr[i];
+        }
+        vector<int> ans(queries.size());
+        for(int k=0;k<queries.size();k++) 
+        {
+            int i=queries[k][0],j=queries[k][1];
+            if(i==0) 
+            {
+                ans[k]=pre[j];
+            } 
+            else 
+            {
+                ans[k]=pre[j]^pre[i-1];
+            }
+        }
+        return ans;
+    }
+};
