48c6fbd005c6df07ab69e6ae70367e0b4e4c6dc3
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2024-09-29 20:55:50 +0530

    Create 0432-All_O`one_Data_Structure.cpp

---
+++ Design/0432-All_O`one_Data_Structure.cpp
@@ -0,0 +1,47 @@
+class AllOne {
+public:
+    unordered_map<string,int> m;
+    set<pair<int,string>> s;
+    AllOne() 
+    {
+        m.clear();
+    }
+    void inc(string key) 
+    {
+        int n=m[key];
+        m[key]++;
+        s.erase({n,key});
+        s.insert({n+1,key});
+    }
+    void dec(string key) 
+    {
+        int n=m[key];
+        m[key]--;
+        s.erase({n,key});
+        if(m[key]>0)
+        {
+            s.insert({n-1,key});
+        }
+        else
+        {
+            m.erase(key);
+        }
+    }
+    string getMaxKey() 
+    {
+        return (!s.empty())?s.rbegin()->second:"";
+    }
+    string getMinKey() 
+    {
+        return (!s.empty())?s.begin()->second:"";
+    }
+};
+
+/**
+ * Your AllOne object will be instantiated and called as such:
+ * AllOne* obj = new AllOne();
+ * obj->inc(key);
+ * obj->dec(key);
+ * string param_3 = obj->getMaxKey();
+ * string param_4 = obj->getMinKey();
+ */
