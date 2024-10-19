0a1141b8fc4449cdcfb4826741b0cc96fe3c875f
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2024-10-19 18:37:26 +0530

    Create 1545-Find_Kth_Bit_in_Nth_Binary_String.cpp

---
+++ String/1545-Find_Kth_Bit_in_Nth_Binary_String.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    char findKthBit(int n,int k) 
+    {
+        if(n==1)
+        {
+            return '0';
+        }
+        int l=(1<<n)-1,mid=l/2+1;
+        if(k==mid)
+        {
+            return '1';
+        }
+        if(k<mid)
+        {
+            return findKthBit(n-1,k);
+        }
+        return (findKthBit(n-1,l-k+1)=='0')?'1':'0';
+    }
+};
