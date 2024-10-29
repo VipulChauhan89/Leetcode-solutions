9c56116d041371bb8155ad62f954d7b6384ceb09
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2024-10-29 21:55:20 +0530

    Create 2684-Maximum_Number_of_Moves_in_a_Grid.cpp

---
+++ Dynamic Programming/2684-Maximum_Number_of_Moves_in_a_Grid.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int maxMoves(vector<vector<int>> &grid) 
+    {
+        int m=grid.size(),n=grid[0].size(),ans=0;
+        vector<int> dp(m);
+        for(int j=1;j<n;j++) 
+        {
+            int leftTop=0;
+            bool found=false;
+            for(int i=0;i<m;i++) 
+            {
+                int cur=-1,nxtLeftTop=dp[i];
+                if(i-1>=0 && leftTop!=-1 && grid[i][j]>grid[i-1][j-1]) 
+                {
+                    cur=max(cur,leftTop+1);
+                }
+                if(dp[i]!=-1 && grid[i][j]>grid[i][j-1]) 
+                {
+                    cur=max(cur,dp[i]+1);
+                }
+                if(i+1<m && dp[i+1]!=-1 && grid[i][j]>grid[i+1][j-1]) 
+                {
+                    cur=max(cur,dp[i+1]+1);
+                }
+                dp[i]=cur;
+                found=found || (dp[i]!=-1);
+                leftTop=nxtLeftTop;
+            }
+            if(!found)
+            {
+                break;
+            } 
+            ans=j;
+        }
+        return ans;
+    }
+};
