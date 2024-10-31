1deffffe0ec278ef4f8afe5b3c28466abf9be8af
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2024-10-31 20:03:05 +0530

    Create 2463-Minimum_Total_Distance_Traveled.cpp

---
+++ Dynamic Programming/2463-Minimum_Total_Distance_Traveled.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    long long minimumTotalDistance(vector<int> &robot,vector<vector<int>> &factory) 
+    {
+        sort(robot.begin(),robot.end());
+        sort(factory.begin(),factory.end());
+        int m=robot.size(),n=factory.size();
+        vector<vector<long long>> dp(m+1,vector<long long>(n+1));
+        for(int i=0;i<m;i++) 
+        {
+            dp[i][n]=LLONG_MAX;
+        }
+        for(int j=n-1;j>=0;j--) 
+        {
+            long long prefix=0;
+            deque<pair<int,long long>> q;
+            q.push_back({m,0});
+            for(int i=m-1;i>=0;i--) 
+            {
+                prefix+=abs(robot[i]-factory[j][0]);
+                while(!q.empty() && q.front().first>i+factory[j][1]) 
+                {
+                    q.pop_front();
+                }
+                while(!q.empty() && q.back().second>=dp[i][j+1]-prefix) 
+                {
+                    q.pop_back();
+                }
+                q.push_back({i,dp[i][j+1]-prefix});
+                dp[i][j]=q.front().second+prefix;
+            }
+        }
+        return dp[0][0];
+    }
+};
