b11a7bcfc01cfdda9209ead5f39ffaaa693b3dfc
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2024-11-18 22:01:20 +0530

    Create 1652-Defuse_the_Bomb.cpp

---
+++ Array/1652-Defuse_the_Bomb.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> decrypt(vector<int> &code,int k) 
+    {
+        int n=code.size();
+        vector<int> ans(n,0);
+        if(k==0)
+        {
+            return ans;
+        }
+        if(k>0)
+        {
+            int sum=accumulate(code.begin()+1,code.begin()+k+1,0);
+            ans[0]=sum;
+            for(int i=1,j=k+1;i<n;i++,j++)
+            {
+                sum+=-code[i]+code[j%n];
+                ans[i]=sum;
+            }
+            return ans;
+        }
+        k=-k;
+        int sum=accumulate(code.end()-k,code.end(),0);
+        ans[0]=sum;
+        for(int i=0,j=n-k;i<n-1;i++,j++)
+        {
+            sum+=-code[j%n]+code[i];
+            ans[i+1]=sum;
+        }
+        return ans;
+    }
+};
