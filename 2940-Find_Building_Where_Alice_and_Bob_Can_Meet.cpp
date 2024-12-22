14fb680dbc4be85875176910055a2507a2398774
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2024-12-22 21:47:05 +0530

    Create 2940-Find_Building_Where_Alice_and_Bob_Can_Meet.cpp

---
+++ 2940-Find_Building_Where_Alice_and_Bob_Can_Meet.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<int> leftmostBuildingQueries(vector<int> &heights,vector<vector<int>> &queries) 
+    {
+        int n=heights.size(),qz=queries.size();
+        vector<int> ans(qz,-1);
+        vector<pair<int,int>> idx;
+        for(int i=0;i<qz;i++) 
+        {
+            int &x=queries[i][0],&y=queries[i][1];
+            if(x>y)
+            {
+                swap(x,y);
+            }
+            if(x==y|| heights[x]<heights[y])
+            {
+                ans[i]=y;
+            }
+            else
+            {
+                idx.emplace_back(y,i);
+            }
+        }
+        sort(idx.begin(),idx.end(),greater<>());
+        vector<pair<int,int>> stack;
+        int j=n-1;
+        for(auto [_,i]:idx) 
+        {
+            int x=queries[i][0],y=queries[i][1];
+            for(;j>y;j--) 
+            {
+                while(!stack.empty() && heights[stack.back().second]<heights[j])
+                {
+                    stack.pop_back();
+                }
+                stack.emplace_back(heights[j],j);
+            }
+            auto it=upper_bound(stack.rbegin(),stack.rend(),make_pair(heights[x],n));
+            ans[i]=(it==stack.rend())?-1:it->second;
+        }
+        return ans;
+    }
+};
