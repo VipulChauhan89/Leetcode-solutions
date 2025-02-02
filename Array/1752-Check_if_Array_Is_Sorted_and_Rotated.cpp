574706fb13d59a1d99b8b2b236184ba1a85a4b1f
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2025-02-02 09:33:52 +0530

    Update 1752-Check_if_Array_Is_Sorted_and_Rotated.cpp

--- Array/1752-Check_if_Array_Is_Sorted_and_Rotated.cpp
+++ Array/1752-Check_if_Array_Is_Sorted_and_Rotated.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
-    bool check(vector<int>& nums) {
-        int count=0;
-        for(int i=1;i<nums.size();i++)
+    bool check(vector<int> &nums) 
+    {
+        int n=nums.size(),count=0;
+        for(int i=0;i<n;i++)
         {
-            if(nums[i-1]>nums[i])
+            if(nums[i]>nums[(i+1)%n])
             {
                 count++;
             }
+            if(count>1)
+            {
+                break;
+            }
         }
-        if(count==1)
-        {
-            return (nums[0]>=nums[nums.size()-1])?true:false;
-        }
-        else if(count==0)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        return count<=1;
     }
 };
