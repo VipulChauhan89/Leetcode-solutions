371d88de715872c7c3e5dab19b207b4c342154cc
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2025-02-07 01:45:18 +0530

    Create 3289-The_Two_Sneaky_Numbers_of_Digitville.cpp

---
+++ Array/3289-The_Two_Sneaky_Numbers_of_Digitville.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> getSneakyNumbers(vector<int> &nums) 
+    {
+        vector<int> ans;
+        unordered_map<int,int> m;
+        for(auto &i:nums)
+        {
+            m[i]++;
+            if(m[i]==2)
+            {
+                ans.push_back(i);
+            }
+        }    
+        return ans;
+    }
+};
