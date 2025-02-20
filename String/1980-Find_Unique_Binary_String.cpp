8b1464ce05af13f75b9fd7ad80eef38b505b2f5a
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2025-02-20 22:51:40 +0530

    Update 1980-Find_Unique_Binary_String.cpp

--- String/1980-Find_Unique_Binary_String.cpp
+++ String/1980-Find_Unique_Binary_String.cpp
@@ -2,8 +2,9 @@ class Solution {
 public:
     string findDifferentBinaryString(vector<string> &nums) 
     {
+        int n=nums.size();
         string ans="";
-        for(int i=0;i<nums.size();i++)
+        for(int i=0;i<n;i++)
         {
             ans+=(nums[i][i]=='0'?'1':'0');
         }
