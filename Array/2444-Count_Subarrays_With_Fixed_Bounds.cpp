ed545073ac2bce3821233980ca8fcf96efa77de9
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2025-04-26 19:57:56 +0530

    Update 2444-Count_Subarrays_With_Fixed_Bounds.cpp

--- Array/2444-Count_Subarrays_With_Fixed_Bounds.cpp
+++ Array/2444-Count_Subarrays_With_Fixed_Bounds.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
-    long long countSubarrays(vector<int>& nums, int minK, int maxK) 
+    long long countSubarrays(vector<int> &nums,int minK,int maxK) 
     {
         long long ans=0;
         int badi=-1,mn=-1,mx=-1,n=nums.size();
-
         for(int i=0;i<n;i++)
         {
             if(nums[i]<minK || nums[i]>maxK)
