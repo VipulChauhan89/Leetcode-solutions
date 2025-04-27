56fd6e12f481b9fb7327fe52cde93c5f2d1eff9a
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2025-04-27 16:44:38 +0530

    Update 3392-Count_Subarrays_of_Length_Three_With_a_Condition.cpp

--- Array/3392-Count_Subarrays_of_Length_Three_With_a_Condition.cpp
+++ Array/3392-Count_Subarrays_of_Length_Three_With_a_Condition.cpp
@@ -2,13 +2,10 @@ class Solution {
 public:
     int countSubarrays(vector<int> &nums) 
     {
-        int count=0,n=nums.size();
-        for(int i=2;i<n;i++)
+        int count=0,n=nums.size()-1;
+        for(int i=1;i<n;i++)
         {
-            if((nums[i-2]+nums[i])*2==nums[i-1])
-            {
-                count++;
-            }
+            count+=(nums[i]%2==0 && nums[i]/2==nums[i-1]+nums[i+1]);
         }
         return count;
     }
