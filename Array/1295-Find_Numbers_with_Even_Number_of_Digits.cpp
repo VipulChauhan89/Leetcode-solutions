f66e2a412f30f643492e3e54911320b40321e9c9
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2025-04-30 19:44:06 +0530

    Create 1295-Find_Numbers_with_Even_Number_of_Digits.cpp

---
+++ Array/1295-Find_Numbers_with_Even_Number_of_Digits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findNumbers(vector<int> &nums) 
+    {
+        int count=0;
+        for(auto &i:nums)
+        {
+            string s=to_string(i);
+            if(s.size()%2==0)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+};
