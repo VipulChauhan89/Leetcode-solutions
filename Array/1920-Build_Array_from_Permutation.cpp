19b70087858c9caf0270a2214b83f9c0c12366f7
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2025-05-06 23:54:34 +0530

    Update 1920-Build_Array_from_Permutation.cpp

--- Array/1920-Build_Array_from_Permutation.cpp
+++ Array/1920-Build_Array_from_Permutation.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    vector<int> buildArray(vector<int>& nums) 
+    vector<int> buildArray(vector<int> &nums) 
     {
-        vector<int> v;
-        for(auto i:nums)
+        vector<int> ans;
+        for(auto &i:nums)
         {
-            v.push_back(nums[i]);
+            ans.push_back(nums[i]);
         }
-        return v;
+        return ans;
     }
 };
