07078e45f1c102a9799a6abd7bd0e747c1dfa9f2
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2025-06-05 18:03:30 +0530

    Update 1061-Lexicographically_Smallest_Equivalent_String.cpp

--- String/1061-Lexicographically_Smallest_Equivalent_String.cpp
+++ String/1061-Lexicographically_Smallest_Equivalent_String.cpp
@@ -2,27 +2,26 @@ class Solution {
 public:
     string smallestEquivalentString(string s1,string s2,string baseStr) 
     {
-        int mx,mn;
-        vector<int> m(26);
+        vector<char> a(26);
         for(int i=0;i<26;i++)
         {
-          m[i]='a'+i;
+            a[i]='a'+i;
         }
         for(int i=0;i<s1.size();i++)
         {
-            mx=max(m[s1[i]-'a'],m[s2[i]-'a']);
-            mn=min(m[s1[i]-'a'],m[s2[i]-'a']);
-            for(int i=0;i<26;i++)
+            char t1=a[s1[i]-'a'],t2=a[s2[i]-'a'];
+            char mn=min(t1,t2);
+            for(int j=0;j<26;j++)
             {
-                if(m[i]==mx)
+                if(a[j]==t1 || a[j]==t2)
                 {
-                    m[i]=mn;
+                    a[j]=mn;
                 }
             }
         }
         for(int i=0;i<baseStr.size();i++)
         {
-            baseStr[i]=m[baseStr[i]-'a'];
+            baseStr[i]=a[baseStr[i]-'a'];
         }
         return baseStr;
     }
