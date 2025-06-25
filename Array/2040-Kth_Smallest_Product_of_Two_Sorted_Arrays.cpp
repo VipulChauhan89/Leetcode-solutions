93d8c14a6a2059e5c9fa227368694059cdc296bf
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2025-06-25 22:20:21 +0530

    Update 2040-Kth_Smallest_Product_of_Two_Sorted_Arrays.cpp

--- Array/2040-Kth_Smallest_Product_of_Two_Sorted_Arrays.cpp
+++ Array/2040-Kth_Smallest_Product_of_Two_Sorted_Arrays.cpp
@@ -9,21 +9,21 @@ public:
             if(countProducts(nums1,nums2,mid)<k)
             {
                 l=mid+1;
-            }   
+            }
             else
             {
                 r=mid;
-            } 
+            }
         }
         return l;
     }
 
-    long long countProducts(vector<int> &nums1,vector<int> &nums2,long long target) 
+    long long countProducts(vector<int> &nums1,vector<int> &nums2,long long target)
     {
         long long count=0;
         for(auto &i:nums1)
         {
-            if(num1==0)
+            if(i==0)
             {
                 if(target>=0)
                 {
@@ -31,36 +31,36 @@ public:
                 }
                 continue;
             }
+
             int l=0,r=nums2.size();
             while(l<r)
             {
                 int mid=(l+r)/2;
-                long long prod=1LL*num1*nums2[mid];
+                long long prod=1LL*i*nums2[mid];
                 if(prod<=target)
                 {
-                    if(num1>0)
+                    if(i>0)
                     {
                         l=mid+1;
-                    } 
+                    }
                     else
                     {
                         r=mid;
-                    } 
+                    }
                 }
                 else
                 {
-                    if(num1>0)
+                    if(i>0)
                     {
                         r=mid;
                     }
                     else
                     {
-
+                        l=mid+1;
                     }
-                    l=mid+1;
                 }
             }
-            count+=(num1>0)?l:(nums2.size()-l);
+            count+=(i>0)?l:(nums2.size()-l);
         }
         return count;
     }
