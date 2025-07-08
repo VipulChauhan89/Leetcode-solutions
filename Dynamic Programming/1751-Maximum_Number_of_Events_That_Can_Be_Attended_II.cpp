a4c5fdda403cae114bc6bbc8cc7ffb099a5d3fb5
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2025-07-08 22:03:24 +0530

    Update 1751-Maximum_Number_of_Events_That_Can_Be_Attended_II.cpp

--- Dynamic Programming/1751-Maximum_Number_of_Events_That_Can_Be_Attended_II.cpp
+++ Dynamic Programming/1751-Maximum_Number_of_Events_That_Can_Be_Attended_II.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-vector<vector<int>>dp;
-    int maxValue(vector<vector<int>> &events,int k) 
+    int attend(int i,int num,vector<vector<int>> &events,int n,vector<int> &next,vector<vector<int>> &dp)
     {
-        sort(events.begin(),events.end());
-        int n=events.size();
-        dp.resize(n,vector<int>(k+1,-1));
-        return solve(events,0,k,0);
-    }
-    int solve(vector<vector<int>> &events,int i,int k,int end)
-    {
-        if(i==events.size() ||  k==0)
+        if(i==n || num==0)
         {
             return 0;
         }
-        if(events[i][0]<=end)
+        if(dp[i][num]!=-1)
         {
-            return solve(events,i+1,k,end);
-        } 
-        if(dp[i][k]!=-1)
+            return dp[i][num];
+        }
+        int a=events[i][2]+attend(next[i],num-1,events,n,next,dp),s=attend(i+1,num,events,n,next,dp),ans=max(a,s);
+        return dp[i][num]=ans;
+    }
+    int maxValue(vector<vector<int>> &events,int k)
+    {
+        int n=events.size();
+        vector<int> next(n,n);
+        vector<vector<int>> dp(n,vector<int>(k+1,-1));
+        sort(events.begin(),events.end());
+        for(int i=0;i<n;i++)
         {
-            return dp[i][k];
-        } 
-        int choices=max(events[i][2]+solve(events,i+1,k-1,events[i][1]),solve(events,i+1,k,end));
-        return dp[i][k]=choices;
+            next[i]=upper_bound(events.begin()+i,events.end(),vector<int>{events[i][1]+1,0,0})-events.begin();
+        }
+        return attend(0,k,events,n,next,dp);
     }
 };
