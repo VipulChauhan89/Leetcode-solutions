1524f8306ef823ce064accf10549b269c00b94dc
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2025-08-01 18:10:30 +0530

    Update 0118-Pascal's_Triangle.cpp

--- Dynamic Programming/0118-Pascal's_Triangle.cpp
+++ Dynamic Programming/0118-Pascal's_Triangle.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    vector<vector<int>> generate(int numRows) 
+    vector<vector<int>> generate(int numRows)
     {
         vector<vector<int>> ans(numRows,vector<int>(1,1));
         for(int i=1;i<numRows;i++)
         {
             ans[i].resize(i+1,1);
-            for(int j=1;j<i;j++)
+            for(int j=1;j<ans[i].size()-1;j++)
             {
                 ans[i][j]=ans[i-1][j-1]+ans[i-1][j];
             }
