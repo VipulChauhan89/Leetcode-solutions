7cf2a448a8bedc3ecef5eb3a9ce29539482285f6
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2025-08-08 20:30:28 +0530

    Update 0808-Soup_Servings.cpp

--- Dynamic Programming/0808-Soup_Servings.cpp
+++ Dynamic Programming/0808-Soup_Servings.cpp
@@ -1,35 +1,32 @@
 class Solution {
 public:
-    vector<vector<double>> dp;
-    double dfs(int A,int B)
+    double calculate(vector<vector<double>> &dp,int a,int b)
     {
-        if(A<=0 && B>0)
-        {
-            return 1;
-        }
-        if(A<=0 && B<=0)
+        if(a<=0 && b<=0)
         {
             return 0.5;
         }
-        if(A>0 && B<=0)
+        if(a<=0)
         {
-            return 0;
+            return 1.0;
         }
-        if(dp[A][B]!=-1)
+        if(b<=0)
         {
-            return dp[A][B];
+            return 0.0;
         }
-        double ans=0.25*(dfs(A-4,B)+dfs(A-3,B-1)+dfs(A-2, B-2)+dfs(A-1, B-3));
-        return dp[A][B]=ans;
+        if(dp[a][b]!=-1)
+        {
+            return dp[a][b];
+        }
+        dp[a][b]=0.25*(calculate(dp,a-100,b)+calculate(dp,a-75,b-25)+calculate(dp,a-50,b-50)+calculate(dp,a-25,b-75));
+        return dp[a][b];
     }
-    double soupServings(int n) 
-    {
-        if(n>5000)
+    double soupServings(int n) {
+        if(n>=4300)
         {
-            return 1;
+            return 1.0;
         }
-        dp.assign(201,vector<double>(201,-1));
-        int N=ceil(n/25.0);
-        return dfs(N,N);
+        vector<vector<double>> dp(n+1,vector<double>(n+1,-1));
+        return calculate(dp,n,n);
     }
 };
