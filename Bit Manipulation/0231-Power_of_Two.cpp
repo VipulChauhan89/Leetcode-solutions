1cd472ba4df73ddefc111a3f70c24717f090e737
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2025-08-09 19:23:44 +0530

    Update 0231-Power_of_Two.cpp

--- Bit Manipulation/0231-Power_of_Two.cpp
+++ Bit Manipulation/0231-Power_of_Two.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        return ((n<=0)?false:((n&(n-1))==0));
+        if(n<=0)
+        {
+            return false;
+        }
+        return (n & (n-1))==0;
     }
 };
