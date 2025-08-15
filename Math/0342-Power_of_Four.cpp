59b3446e60decea82ab0f54b1402edde850ed27e
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2025-08-15 20:58:31 +0530

    Update 0342-Power_of_Four.cpp

--- Math/0342-Power_of_Four.cpp
+++ Math/0342-Power_of_Four.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
-    bool isPowerOfFour(int n)  
+    bool isPowerOfFour(int n)
     {
-        if(n<=0) 
+        if(n==1)
+        {
+            return true;
+        }
+        if(n<=0 || n%4!=0)
         {
             return false;
         }
-        while(n>1) 
-        {
-            if(n%4!=0) 
-            {
-                return false;
-            }
-            n/=4;
-        }
-        return true;
+        return isPowerOfFour(n/4);
     }
 };
