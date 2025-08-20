21aacf21235e32d46bb37e80b94b37c57d5fe33c
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2025-08-20 21:37:03 +0530

    Update 1277-Count_Square_Submatrices_with_All_Ones.cpp

--- Dynamic Programming/1277-Count_Square_Submatrices_with_All_Ones.cpp
+++ Dynamic Programming/1277-Count_Square_Submatrices_with_All_Ones.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
-    int countSquares(vector<vector<int>> &matrix) 
+    int countSquares(vector<vector<int>> &matrix)
     {
-        int n=matrix.size(),m=matrix[0].size(),ans=0;
+        int n=matrix.size(),m=matrix[0].size(),total=0;
         vector<vector<int>> dp(n,vector<int>(m,0));
-        for(int i=0;i<n;i++) 
+        for(int i=0;i<n;i++)
         {
-            dp[i][0]=matrix[i][0];
-            ans+=dp[i][0];
-        }
-        for(int j=1;j<m;j++) 
-        {
-            dp[0][j]=matrix[0][j];
-            ans+=dp[0][j];
-        }
-        for(int i=1;i<n;i++) 
-        {
-            for(int j=1;j<m;j++) 
+            for(int j=0;j<m;j++)
             {
-                if(matrix[i][j]==1) 
+                if(matrix[i][j]==1)
                 {
-                    dp[i][j]=1+min({dp[i][j-1],dp[i-1][j],dp[i-1][j-1]});
+                    if(i==0 || j==0)
+                    {
+                        dp[i][j]=1;
+                    }
+                    else
+                    {
+                        dp[i][j]=1+min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]});
+                    }
+                    total+=dp[i][j];
                 }
-                ans+=dp[i][j];
             }
         }
-        return ans;
+        return total;
     }
 };
