532eb7eea103be92423c162ed95a262d1e54177c
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2025-09-01 22:36:10 +0530

    Update 1792-Maximum_Average_Pass_Ratio.cpp

--- Array/1792-Maximum_Average_Pass_Ratio.cpp
+++ Array/1792-Maximum_Average_Pass_Ratio.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
-    double maxAverageRatio(vector<vector<int>> &classes,int extraStudents) 
+    double gain(double p,double t)
     {
-        priority_queue<tuple<double,int,int>> heap;
-        double sum=0.0;
-        for(auto &c:classes) 
+        return (p+1)/(t+1)-p/t;
+    }
+    double maxAverageRatio(vector<vector<int>> &classes,int extraStudents)
+    {
+        priority_queue<tuple<double,int,int>> pq;
+        for(auto &i:classes)
         {
-            int p=c[0],q=c[1];
-            sum+=(double)p/q;
-            double improvement=(double)(q-p)/(q*(q+1.0));
-            heap.emplace(improvement,p,q);
+            pq.push({gain(i[0],i[1]),i[0],i[1]});
         }
-        for(int i=0;i<extraStudents;i++) 
+        while(extraStudents--)
         {
-            auto [r,p,q]=heap.top();
-            heap.pop();
-            if(r==0)
-            {
-                break;
-            } 
-            sum+=r;
-            double newImprovement=(double)(q-p)/((q+1.0)*(q+2.0));
-            heap.emplace(newImprovement,p+1,q+1);
+            auto [g,p,t]=pq.top();
+            pq.pop();
+            p++;
+            t++;
+            pq.push({gain(p,t),p,t});
         }
-        return sum/classes.size();
+        double total=0;
+        while(!pq.empty())
+        {
+            auto [g,p,t]=pq.top();
+            pq.pop();
+            total+=(double)p/t;
+        }
+        return total/classes.size();
     }
 };
