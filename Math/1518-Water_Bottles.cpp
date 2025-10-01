77804d9c260a92bd624dd7c99acf762e3ee46c80
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2025-10-01 23:47:37 +0530

    Update 1518-Water_Bottles.cpp

--- Math/1518-Water_Bottles.cpp
+++ Math/1518-Water_Bottles.cpp
@@ -1,13 +1,7 @@
 class Solution {
 public:
-    int numWaterBottles(int numBottles,int numExchange) 
+    int numWaterBottles(int numBottles,int numExchange)
     {
-        int totalBottles=numBottles;
-        while(numBottles>=numExchange)
-        {
-            totalBottles+=numBottles/numExchange;
-            numBottles=(numBottles/numExchange)+(numBottles%numExchange);
-        }
-        return totalBottles;
+        return numBottles+(numBottles-1)/(numExchange-1);
     }
 };
