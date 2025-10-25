65bc317c4ec88c99112daf17910a2060bda59fbf
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2025-10-25 23:33:07 +0530

    Update 1716-Calculate_Money_in_Leetcode_Bank.cpp

--- Math/1716-Calculate_Money_in_Leetcode_Bank.cpp
+++ Math/1716-Calculate_Money_in_Leetcode_Bank.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
-    int totalMoney(int n) 
+    int ap(int lead,int last,int terms)
     {
-        int week_count=n/7,remaining=n%7;
-        int total=((week_count *(week_count-1))/2)*7;
-        total+=week_count*28;
-        total+=((remaining*(remaining+1))/2)+(week_count*remaining);
-        return total;
+        return (lead+last)*terms/2;
+    }
+    int totalMoney(int n)
+    {
+        auto [q,r]=div(n,7);
+        return ap(28,28+(q-1)*7,q)+ap(q+1,q+r,r);
     }
 };
