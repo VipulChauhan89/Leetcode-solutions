db29d92adb7b3816f41228115588c5e3baca6123
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2025-11-08 23:18:13 +0530

    Update 1611-Minimum_One_Bit_Operations_to_Make_Integers_Zero.cpp

--- Bit Manipulation/1611-Minimum_One_Bit_Operations_to_Make_Integers_Zero.cpp
+++ Bit Manipulation/1611-Minimum_One_Bit_Operations_to_Make_Integers_Zero.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    int minimumOneBitOperations(int n) 
+    int minimumOneBitOperations(int n)
     {
         int ans=0;
-        while(n>0)
+        while(n)
         {
-            ans=-(ans+(n^(n-1)));
-            n&=n-1;
-        }    
-        return abs(ans);
+            ans^=n;
+            n>>=1;
+        }
+        return ans;
     }
 };
