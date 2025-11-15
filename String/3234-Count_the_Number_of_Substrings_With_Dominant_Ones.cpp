25ad5fde38acd2503d71d22a7bf1f6f45c5031f7
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2025-11-15 22:11:05 +0530

    Create 3234-Count_the_Number_of_Substrings_With_Dominant_Ones.cpp

---
+++ String/3234-Count_the_Number_of_Substrings_With_Dominant_Ones.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int numberOfSubstrings(string s)
+    {
+        int n=s.size(),ans=0,sid=0;
+        vector<int> zero;
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='0')
+            {
+                zero.push_back(i);
+            }
+        }
+        int ones=n-zero.size(); 
+        zero.push_back(n);
+        for(int left=0;left<n;left++)
+        {
+            for(int id=sid;id<zero.size()-1;id++)
+            {
+                int cnt0=id-sid+1; 
+                if(cnt0*cnt0>ones)
+                {
+                    break;
+                }
+                int p=zero[id],q=zero[id+1],cnt1=zero[id]-left-(id-sid);
+                if(cnt1>=cnt0*cnt0)
+                {
+                    ans+=q-p;
+                }
+                else
+                {
+                    ans+=max(q-p-(cnt0*cnt0-cnt1),0);
+                }
+            }
+            if(s[left]=='0')
+            {
+                sid++;
+            }
+            else
+            {
+                ans+=zero[sid]-left;
+                ones--;
+            }
+        }
+        return ans;
+    }
+};
