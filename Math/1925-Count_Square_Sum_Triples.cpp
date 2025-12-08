0db13af5e7e28a2a3322cf216fd2f4c2896b7099
Vipulchauhan89 <vipulchauhan548@gmail.com>
2025-12-08 09:17:35 +0530

    Created 1925-Count_Square_Sum_Triples.cpp

---
+++ Math/1925-Count_Square_Sum_Triples.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int countTriples(int n)
+    {
+        int ans=0;
+        for(int u=2;u<=sqrt(n);u++)
+        {
+            for(int v=1;v<u;v++)
+            {
+                if(~(u-v) & 1 || gcd(u,v)!=1) 
+                {
+                    continue;
+                }
+                int c=u*u+v*v;
+                if(c>n)
+                {
+                    continue;
+                }
+                ans+=(n/c)<<1;
+            }
+        }
+        return ans;
+    }
+};
