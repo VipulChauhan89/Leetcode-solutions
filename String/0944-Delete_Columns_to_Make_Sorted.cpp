e59108ef8e7186f784cb5d4d1cc1ea2e3ca8d74e
Vipulchauhan89 <vipulchauhan548@gmail.com>
2025-12-20 19:42:10 +0530

    Updated 0944-Delete_Columns_to_Make_Sorted.cpp

--- String/0944-Delete_Columns_to_Make_Sorted.cpp
+++ String/0944-Delete_Columns_to_Make_Sorted.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    int minDeletionSize(vector<string>& strs) 
+    int minDeletionSize(vector<string> &strs)
     {
-        int count=0;
-        for(int i=0;i<strs[0].size();i++)
+        int rows=strs.size(),cols=strs[0].size(),del=0;
+        for(int i=0;i<cols;i++)
         {
-            for(int j=0;j<strs.size()-1;j++)
+            for(int j=0;j<rows-1;j++)
             {
                 if(strs[j][i]>strs[j+1][i])
                 {
-                    count++;
+                    del++;
                     break;
                 }
             }
         }
-        return count;
+        return del;
     }
 };
