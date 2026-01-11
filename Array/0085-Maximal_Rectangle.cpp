09d6a5ec37096435cd151e03b1cb13558056d1f4
Vipulchauhan89 <vipulchauhan548@gmail.com>
2026-01-11 21:20:42 +0530

    Updated 0085-Maximal_Rectangle.cpp

--- Array/0085-Maximal_Rectangle.cpp
+++ Array/0085-Maximal_Rectangle.cpp
@@ -1,30 +1,47 @@
 class Solution {
 public:
-    int maximalRectangle(vector<vector<char>> &matrix) 
+    int largestRectangleArea(vector<int> &heights)
     {
-        if(matrix.empty() || matrix[0].empty())
+        stack<int> st;
+        heights.push_back(0);
+        int maxArea=0;
+        for(int i=0;i<heights.size();i++)
+        {
+            while(!st.empty() && heights[st.top()]>heights[i])
+            {
+                int h=heights[st.top()];
+                st.pop();
+                int width=st.empty()?i:i-st.top()-1;
+                maxArea=max(maxArea,h*width);
+            }
+            st.push(i);
+        }
+        heights.pop_back();
+        return maxArea;
+    }
+    int maximalRectangle(vector<vector<char>> &matrix)
+    {
+        if(matrix.empty())
         {
             return 0;
         }
-        int rows=matrix.size(),cols=matrix[0].size(),maxArea=0;
-        vector<int> heights(cols+1,0);
-        for(auto &row:matrix) 
+        int rows=matrix.size(),cols=matrix[0].size(),ans=0;
+        vector<int> height(cols,0);
+        for(int i=0;i<rows;i++)
         {
-            for(int i=0;i<cols;i++) 
+            for(int j=0;j<cols;j++)
             {
-                heights[i]=(row[i]=='1')?heights[i]+1:0;
-            }
-            int n=heights.size();
-            for(int i=0;i<n;i++) 
-            {
-                for(int j=i,minHeight=INT_MAX;j<n;j++) 
+                if(matrix[i][j]=='1')
                 {
-                    minHeight=min(minHeight,heights[j]);
-                    int area=minHeight*(j-i+1);
-                    maxArea=max(maxArea,area);
+                    height[j]+=1;
+                }
+                else
+                {
+                    height[j]=0;
                 }
             }
+            ans=max(ans,largestRectangleArea(height));
         }
-        return maxArea;
+        return ans;
     }
 };
