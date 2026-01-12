3d3bb5b6db96b60347b71bf9134c8ae6eea51853
Vipulchauhan89 <vipulchauhan548@gmail.com>
2026-01-12 21:32:13 +0530

    Updated 1266-Minimum_Time_Visiting_All_Points.cpp

--- Math/1266-Minimum_Time_Visiting_All_Points.cpp
+++ Math/1266-Minimum_Time_Visiting_All_Points.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    int minTimeToVisitAllPoints(vector<vector<int>> &points) 
+    int minTimeToVisitAllPoints(vector<vector<int>> &points)
     {
-        int time=0;
-        for(int i=1;i<points.size();i++)
+        int ans=0,n=points.size();
+        for(int i=1;i<n;i++)
         {
-            time+=max(abs(points[i-1][0]-points[i][0]),abs(points[i-1][1]-points[i][1]));
-        }   
-        return time; 
+            ans+=max(abs(points[i][0]-points[i-1][0]),abs(points[i][1]-points[i-1][1]));
+        }
+        return ans;
     }
-};
+}
