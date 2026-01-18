82c34dafb6a7b39f488cfbf2afcb5e43c69b4d39
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2026-01-18 23:32:50 +0530

    Create 1895-Largest_Magic_Square.cpp

---
+++ Array/1895-Largest_Magic_Square.cpp
@@ -0,0 +1,74 @@
+class Solution {
+public:
+    bool isValid(vector<vector<int>> &g,int i,int j,int k)
+    {
+        int sum=0;
+        for(int x=i;x<i+k;x++)
+        {
+            int s=0;
+            for(int y=j;y<j+k;y++)
+            {
+                s+=g[x][y];
+            }
+            if(x==i)
+            {
+                sum=s;
+            }
+            else if(sum!=s)
+            {
+                return false;
+            }
+        }
+        for(int y=j;y<j+k;y++)
+        {
+            int s=0;
+            for(int x=i;x<i+k;x++)
+            {
+                s+=g[x][y];
+            }
+            if(sum!=s)
+            {
+                return false;
+            }
+        }
+        int s=0;
+        for(int d=0;d<k;d++)
+        {
+            s+=g[i+d][j+d];
+        }
+        if(sum!=s)
+        {
+            return false;
+        }
+        s=0;
+        for(int d=0;d<k;d++)
+        {
+            s+=g[i+d][j+k-1-d];
+        }
+        if(sum!=s)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    int largestMagicSquare(vector<vector<int>> &grid)
+    {
+        int m=grid.size(),n=grid[0].size();
+        int ans=1;
+        for(int k=2;k<=min(m,n);k++)
+        {
+            for(int i=0;i+k<=m;i++)
+            {
+                for(int j=0;j+k<=n;j++)
+                {
+                    if(isValid(grid,i,j,k))
+                    {
+                        ans=k;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
