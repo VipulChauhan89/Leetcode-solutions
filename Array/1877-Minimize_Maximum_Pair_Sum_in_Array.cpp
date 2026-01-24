74f811bbbd208ee4e218073daca7593a5f0c1e43
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2026-01-24 23:53:05 +0530

    Update 1877-Minimize_Maximum_Pair_Sum_in_Array.cpp

--- Array/1877-Minimize_Maximum_Pair_Sum_in_Array.cpp
+++ Array/1877-Minimize_Maximum_Pair_Sum_in_Array.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    int minPairSum(vector<int> &nums) 
+    int minPairSum(vector<int> &nums)
     {
-        int mx=INT_MIN,n=nums.size();
         sort(nums.begin(),nums.end());
-        for(int i=0;i<n/2;i++)
+        int ans=0,n=nums.size();
+        for(int i=0;i<n>>1;i++)
         {
-            mx=max(mx,nums[i]+nums[n-i-1]);
+            ans=max(ans,nums[n-1-i]+nums[i]);
         }
-        return mx;
+        return ans;
     }
 };
