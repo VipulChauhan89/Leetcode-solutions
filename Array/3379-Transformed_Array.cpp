d005e5c2ed4eee61b72335faa561a04d1c34e0b4
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2026-02-05 23:37:44 +0530

    Create 3379-Transformed_Array.cpp

---
+++ Array/3379-Transformed_Array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> constructTransformedArray(vector<int> &nums)
+    {
+        int n=nums.size();
+        vector<int> ans;
+        for(int i=0;i<n;i++)
+        {
+            ans.push_back(nums[(((i+nums[i])%n)+n)%n]);
+        }
+        return ans;
+    }
+};
