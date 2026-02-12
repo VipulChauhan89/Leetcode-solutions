2c90be3c66d5cae1f29597490c57b56bfaae1a75
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2026-02-12 23:58:53 +0530

    Create 3713-Longest_Balanced_Substring_I.cpp

---
+++ Hash Table/3713-Longest_Balanced_Substring_I.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int longestBalanced(string s)
+    {
+        int n=s.length(),maxLen=0;
+        for(int i=0;i<n;i++)
+        {
+            int freqMap[26]={},maxFreq=0,distincts=0;
+            for(int j=i;j<n;j++)
+            {
+                int idx=s[j]-'a';
+                distincts+=(freqMap[idx]==0);
+                maxFreq=max(maxFreq,++freqMap[idx]);
+                if(j-i+1==distincts*maxFreq)
+                {
+                    maxLen=max(maxLen,j-i+1);
+                }
+            }
+        }
+        return maxLen;   
+    }
+};
