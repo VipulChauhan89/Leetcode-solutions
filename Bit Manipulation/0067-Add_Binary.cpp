f9f9562be2cdc08714e4ae900ba4337d2b763e99
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2026-02-15 21:07:00 +0530

    Create 0067-Add_Binary.cpp

---
+++ Bit Manipulation/0067-Add_Binary.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string addBinary(string a, string b)
+    {
+        string ans="";
+        int i=a.size()-1,j=b.size()-1,carry=0;
+        while(i>=0 || j>=0 || carry)
+        {
+            int sum=carry;
+            if(i>=0)
+            {
+                sum+=a[i]-'0';
+                i--;
+            }
+            if(j>=0)
+            {
+                sum+=b[j]-'0';
+                j--;
+            }
+            ans=char((sum%2)+'0')+ans;
+            carry=sum/2;
+        }
+        return ans;
+    }
+};
