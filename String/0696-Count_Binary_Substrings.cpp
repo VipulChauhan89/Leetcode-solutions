f4d92a5c2686b61855ec94d4fb0f081b224833e7
Vipulchauhan89 <vipulchauhan548@gmail.com>
2026-02-19 12:40:57 +0530

    Created 0696-Count_Binary_Substrings.cpp

---
+++ String/0696-Count_Binary_Substrings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int countBinarySubstrings(string s)
+    {
+        int ans=0,prev=0,strk=1;
+        for(int i=1;i<s.size();i++)
+        {
+            if(s[i]==s[i-1])
+            {
+                strk++;
+            }
+            else
+            {
+                prev=strk;
+                strk=1;
+            }
+            if(strk<=prev)
+            {
+                ans++;
+            }
+        }
+        return ans;    
+    }
+};
