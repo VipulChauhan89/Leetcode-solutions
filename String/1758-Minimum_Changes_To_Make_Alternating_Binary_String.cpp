277693fb9b8d39cde9db0a0023a9e89e5a4d3396
Vipulchauhan89 <vipulchauhan548@gmail.com>
2026-03-05 12:51:16 +0530

    Created 1758-Minimum_Changes_To_Make_Alternating_Binary_String.cpp

--- String/1758-Minimum_Changes_To_Make_Alternating_Binary_String.cpp
+++ String/1758-Minimum_Changes_To_Make_Alternating_Binary_String.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
-    int minOperations(string s) 
+    int minOperations(string s)
     {
-        vector<int> binary(2,0);
-        for(int i=0;i<s.length();i++)
+        int count=0,n=s.length();
+        for(int i=0;i<n;i++)
         {
-            if(i%2==0)
-            {
-                binary[s[i]=='0']++;
-            }
-            else
-            {
-                binary[s[i]=='1']++;
-            }
+            count+=(s[i]^i)&1;
         }
-        return min(binary[0],binary[1]);
+        return min(count,n-count);
     }
 };
