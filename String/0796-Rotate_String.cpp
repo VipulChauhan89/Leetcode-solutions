9acab22b0b58378ae762b3b7fd4381c95e083a55
Vipulchauhan89 <vipulchauhan548@gmail.com>
2026-05-03 15:36:57 +0530

    Updated 0796-Rotate_String.cpp

--- String/0796-Rotate_String.cpp
+++ String/0796-Rotate_String.cpp
@@ -1,22 +1,7 @@
 class Solution {
 public:
-    bool rotateString(string s,string goal) 
+    bool rotateString(string s,string goal)
     {
-        if(s.length()!=goal.length())
-        {
-            return false;
-        }
-        string rotated=s;
-        for(int i=0;i<s.length();i++) 
-        {
-            if(rotated==goal)
-            {
-                return true;
-            }
-            char ch=rotated[0];
-            rotated.erase(0,1);
-            rotated.push_back(ch);
-        }
-        return false;
+        return s.size()==goal.size() && (s+s).find(goal)!=-1;
     }
 };
