83fb4b970df2a24dc7efeb3ae712173955b22ce8
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2026-05-10 22:39:35 +0530

    Create 2770-Maximum_Number_of_Jumps_to_Reach_the_Last_Index.cpp

---
+++ Dynamic Programming/2770-Maximum_Number_of_Jumps_to_Reach_the_Last_Index.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maximumJumps(vector<int> &nums,int target)
+    {
+        int n=nums.size();
+        vector<int> dp(n,-1);
+        dp[0]=0;
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                if(abs(nums[i]-nums[j])<=target && dp[j]>-1)
+                {
+                    dp[i]=max(dp[i],1+dp[j]);
+                }
+            }
+        }
+        return dp[n-1];
+    }
+};
