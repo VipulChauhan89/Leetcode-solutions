2de99e2ab21e357e9f0d20041ac3f6e1006fe6a3
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2026-05-16 23:51:39 +0530

    Create 0154-Find_Minimum_in_Rotated_Sorted_Array_II.cpp

---
+++ 0154-Find_Minimum_in_Rotated_Sorted_Array_II.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findMin(vector<int> &nums)
+    {
+        int n=nums.size()-1,last=nums[n],left=0,right=n;
+        while(left<n && nums[left]==last)
+        {
+            left++;
+        }
+        while(left<right)
+        {
+            int mid=left+right>>1;
+            if(nums[mid]>last)
+            {
+                left=mid+1;
+            }
+            else
+            {
+                right=mid;
+            }
+        }
+        return nums[left];
+    }
+};
