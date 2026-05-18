36793cbd2961f8f3b2754f7fcd2f7d678a2a5524
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2026-05-18 22:10:06 +0530

    Update 1345-Jump_Game_IV.cpp

--- Graph/1345-Jump_Game_IV.cpp
+++ Graph/1345-Jump_Game_IV.cpp
@@ -1,50 +1,49 @@
 class Solution {
 public:
-    int minJumps(vector<int>&arr) 
+    int minJumps(vector<int> &arr)
     {
-        vector<bool>reached_index(arr.size(),false);
-        unordered_map<int,vector<int>>index_of_val;
-        for(int i=0;i<arr.size();i++)
+        int n=arr.size();
+        if(n==1)
         {
-            index_of_val[arr[i]].push_back(i);
+            return 0;
         }
-        queue<int>to_reach;
-        to_reach.push(0);
-        reached_index[0]=true;
-        int steps=0;
-        while(!to_reach.empty())
+        unordered_map<int,vector<int>>mp;
+        for(int i=0;i<n;i++)
         {
-            int n=to_reach.size();
-            while(n--)
+            mp[arr[i]].push_back(i);
+        }
+        queue<pair<int,int>>q;
+        q.push({0,0});
+        vector<int>vis(n,0);
+        vis[0]=1;
+        while(!q.empty())
+        {
+            int node=q.front().first,dist=q.front().second;
+            q.pop();
+            if(node==n-1)
             {
-                int x=to_reach.front();
-                if(x==arr.size()-1)
-                {
-                    return steps;
-                }
-                to_reach.pop();
-                for(auto j:index_of_val[arr[x]])
-                {
-                    if(!reached_index[j])
-                    {
-                        to_reach.push(j);
-                        reached_index[j]=true;
-                    }
-                }
-                if((x-1)>0 && !reached_index[x-1])
-                {
-                    to_reach.push(x-1);
-                    reached_index[x-1]=true;
-                }
-                if((x+1)<arr.size() && !reached_index[x+1])
-                {
-                    to_reach.push(x+1);
-                    reached_index[x+1]=true;
-                }
-                index_of_val[arr[x]].clear();
+                return dist;
             }
-            steps++;
+            if(node-1>=0 && !vis[node-1])
+            {
+                vis[node-1]=1;
+                q.push({node-1,dist+1});
+            }
+            if(node+1<n && !vis[node+1])
+            {
+                vis[node+1]=1;
+                q.push({node+1,dist+1});
+            }
+            for(int next:mp[arr[node]])
+            {
+                if(!vis[next])
+                {
+                    vis[next]=1;
+                    q.push({next,dist+1});
+                }
+            }
+            mp[arr[node]].clear();
         }
-        return arr.size()-1;
+        return -1;
     }
 };
