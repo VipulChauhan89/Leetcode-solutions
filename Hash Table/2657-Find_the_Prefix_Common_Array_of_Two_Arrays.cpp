d82ca4253b2cfc58cda561a06c1d7ed5930c61eb
Vipulchauhan89 <Vipulchauhan548@gmail.com>
2026-05-20 10:51:36 +0530

    Updated 2657-Find_the_Prefix_Common_Array_of_Two_Arrays.cpp

--- Hash Table/2657-Find_the_Prefix_Common_Array_of_Two_Arrays.cpp
+++ Hash Table/2657-Find_the_Prefix_Common_Array_of_Two_Arrays.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
-    vector<int> findThePrefixCommonArray(vector<int>& A, vector<int> &B) 
+    vector<int> findThePrefixCommonArray(vector<int> &A,vector<int> &B)
     {
-        int n=A.size(),common=0;
-        vector<int> ans,count(n,0);
+        int n=A.size(),count=0;
+        vector<int> ans(n);
+        bitset<51> seen;
         for(int i=0;i<n;i++)
         {
-            count[A[i]-1]++;
-            if(count[A[i]-1]==2)
-            {
-                common++;
-            }
-            count[B[i]-1]++;
-            if(count[B[i]-1]==2)
-            {
-                common++;
-            }
-            ans.push_back(common);
+            count+=seen[A[i]];
+            seen.set(A[i]);
+            count+=seen[B[i]];
+            seen.set(B[i]);
+            ans[i]=count;
         }
         return ans;
     }
