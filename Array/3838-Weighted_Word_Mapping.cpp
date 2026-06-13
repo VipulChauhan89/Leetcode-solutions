245290d6e549765cc2a1d41314f49119621e2fac
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2026-06-13 21:11:55 +0530

    Create 3838-Weighted_Word_Mapping.cpp

---
+++ Array/3838-Weighted_Word_Mapping.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string mapWordWeights(vector<string> &words,vector<int> &weights)
+    {
+        string ans(words.size(),0);
+        int i=0;
+        for(auto &word:words)
+        {
+            int s=0;
+            for(auto &c:word)
+            {
+                s+=weights[(c&(1<<5)-1)-1];
+            }
+            ans[i++]='z'-(s-((s*2521)>>(1<<4))*26);
+        }
+        return ans;
+    }
+};
