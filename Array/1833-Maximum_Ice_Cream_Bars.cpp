691e527cca19f8cdb8c35a75ec49d1b25c6bb903
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2026-06-21 21:54:01 +0530

    Update 1833-Maximum_Ice_Cream_Bars.cpp

--- Array/1833-Maximum_Ice_Cream_Bars.cpp
+++ Array/1833-Maximum_Ice_Cream_Bars.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
-    int maxIceCream(vector<int>& costs, int coins) 
+    int maxIceCream(vector<int> &costs,int coins)
     {
-        sort(costs.begin(),costs.end());
-        int i=0;
-        for(;i<costs.size();i++)
+        vector<int> freq(1e5+1);
+        int xMax=0,count=0;
+        for(int x:costs)
         {
-            if(costs[i]>coins)
+            freq[x]++;
+            xMax=max(xMax,x);
+        }
+        for(int x=1;x<=xMax;x++)
+        {
+            const int f=freq[x];
+            if(f==0)
+            {
+                continue;
+            }
+            int buy=min(coins/x,f);
+            if(buy==0)
             {
                 break;
             }
-            coins-=costs[i];
-        }    
-        return i;
+            count+=buy;
+            coins-=buy*x;
+        }
+        for(int x:costs)
+        {
+            freq[x]=0;
+        }
+        return count;
     }
 };
